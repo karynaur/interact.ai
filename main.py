99a6817481f390623a89360b69d90107da0c086f
karynaur <adityasrinivas.menon@gmail.com>
2021-05-02 01:06:47 +0530

    functionize it all :)

--- main.py
+++ main.py
@@ -8,32 +8,32 @@ from pydub import AudioSegment
 import os
 import glob
 
-files = glob.glob('audio/*')
-files.extend(glob.glob('videos/*'))
-if files is not None:
-   for i in files:
-      os.remove(i)
+def main(sentence): 
+  files = glob.glob('audio/*')
+  files.extend(glob.glob('videos/*'))
+  if files is not None:
+     for i in files:
+        os.remove(i)
 
 
-sentence = "jack is a good boy"
-words = []
-for i,word in enumerate(sentence.split()):
-   words.append(cued_speech(word))
-   tts = gTTS(word,lang = 'en', tld = 'ca')
-   tts.save(f'audio/{i}.wav')
-   audio = AudioSegment.from_file(f'audio/{i}.wav')
-   words[i]['length'] = audio.duration_seconds/len(words[i]['word'])
+  words = []
+  for i,word in enumerate(sentence.split()):
+     words.append(cued_speech(word))
+     tts = gTTS(word,lang = 'en', tld = 'ca')
+     tts.save(f'audio/{i}.wav')
+     audio = AudioSegment.from_file(f'audio/{i}.wav')
+     words[i]['length'] = audio.duration_seconds/len(words[i]['word'])
 
 
-   args = argparse.Namespace(audio=f'audio/{i}.wav', box=[-1, -1, -1, -1],\
-   checkpoint_path='Wav2Lip/checkpoints/wav2lip_gan.pth', crop=[0, -1, 0, -1],\
-   face='face.png', face_det_batch_size=16, fps=25.0, img_size=96, nosmooth=False,\
-   outfile=f'videos/{i}.mp4', pads=[0, 10, 0, 0], resize_factor=1, rotate=False, \
-   static=True, wav2lip_batch_size=128)
-   
-   inference.main(args)
+     args = argparse.Namespace(audio=f'audio/{i}.wav', box=[-1, -1, -1, -1],\
+     checkpoint_path='Wav2Lip/checkpoints/wav2lip_gan.pth', crop=[0, -1, 0, -1],\
+     face='face.png', face_det_batch_size=16, fps=25.0, img_size=96, nosmooth=False,\
+     outfile=f'videos/{i}.mp4', pads=[0, 10, 0, 0], resize_factor=1, rotate=False, \
+     static=True, wav2lip_batch_size=128)
+     
+     inference.main(args)
 
-print(words)
+  return words
 
 
 
