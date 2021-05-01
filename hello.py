44667d08049fc75fc8cbb94e8a9280fdbfc984ef
vishal-lokare <vishallokare11502@gmail.com>
2021-05-02 02:50:56 +0530

    added

--- hello.py
+++ hello.py
@@ -3,10 +3,10 @@ from main import main
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/<string:name>/')
+@app.route('/<string:name>/', methods = ['GET'])
 def hello(name):
     x = name.replace("+", " ")
     obj = json.loads(main(x))
     return obj
-app.run()
+app.run(host="192.168.0.106", port="5000")
 
